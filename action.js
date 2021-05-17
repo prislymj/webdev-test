7a30343c173c9e2fa154af2a231352482bc6db7c
Prisly Mary Jacob <72403662+prislymj@users.noreply.github.com>
2021-05-17 22:09:28 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,11 @@
+function purple(){
+    document.body.style.background="purple"
+}
+
+function green(){
+    document.body.style.background="green"
+}
+
+function yellow(){
+    document.body.style.background="yellow"
+}
